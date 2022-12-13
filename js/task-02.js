3b477e916116425b44602f2e66a68b0ceaf529d6
AuroraPolaire <sea.forest.mountains@gmail.com>
2022-12-13 21:32:49 +0200

    Second task completed

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const makeList = (ingredients) => {
+  return ingredients.map((ingredient) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("item");
+    listItem.textContent = ingredient;
+
+    return listItem;
+  });
+};
+
+const list = makeList(ingredients);
+
+ingredientsEl.append(...list);
