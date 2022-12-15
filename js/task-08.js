6444451d284ab4dd0835bc0d05d3e35cbed0b228
AuroraPolaire <sea.forest.mountains@gmail.com>
2022-12-15 23:08:55 +0200

    task 8 finished

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,43 @@
+const submitButton = document.querySelector("button");
+const form = document.querySelector(".login-form");
+const emailPlaceholder = document.getElementsByName("email")[0];
+const passwordPlaceholder = document.getElementsByName("password")[0];
 
+function onSubmitDataCollect(event) {
+  event.preventDefault();
+  const formElement = event.currentTarget.elements;
+  const email = formElement.email.value;
+  const password = formElement.password.value;
+
+  if (email === "") {
+    emailPlaceholder.placeholder = "Field can't be empty";
+    formElement.email.style.border = "3px solid #f44336";
+  }
+  if (password === "") {
+    passwordPlaceholder.placeholder = "Field can't be empty";
+    formElement.password.style.border = "3px solid #f44336";
+  }
+
+  if (email && password) {
+    const formData = {
+      email,
+      password,
+    };
+
+    console.log(formData);
+
+    event.currentTarget.reset();
+
+    emailPlaceholder.placeholder = "";
+    formElement.email.style.border = "1px solid black";
+
+    passwordPlaceholder.placeholder = "";
+    formElement.password.style.border = "1px solid black";
+  }
+}
+
+form.addEventListener("submit", onSubmitDataCollect);
+
+// const body = document.body.children[1];
+// const firstInput = body.children[0].
+// const secondInput = body.children[1].
