38009d70942fa2e90cfd2cdaa2fd120612a5e66d
AuroraPolaire <sea.forest.mountains@gmail.com>
2022-12-15 23:24:51 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return color;
 }
+
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const colorNamePlace = document.querySelector(".color");
+
+function onButtonClickChangeBackground(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  colorNamePlace.textContent = getRandomHexColor();
+}
+
+button.addEventListener("click", onButtonClickChangeBackground);
