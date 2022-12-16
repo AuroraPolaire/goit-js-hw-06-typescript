d3510c1a950b96928a1b0d7c718284193605ceaa
AuroraPolaire <sea.forest.mountains@gmail.com>
2022-12-16 12:14:38 +0200

    task 10 finished

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,44 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const input = document.querySelector("input");
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document.querySelector("button[data-destroy]");
+const boxesPlace = document.querySelector("#boxes");
+
+const inputValue = (number) => {
+  return input.value;
+};
+
+const getInputValue = (event) => {
+  const amount = inputValue();
+  if (!amount) {
+    return;
+  }
+  let box = [];
+  let boxString = "";
+  let size = 30;
+  for (let i = 1; i <= amount; i += 1) {
+    if (i > 1) size += 10;
+    box.push(
+      `<div style="display: absolute; background-color:${getRandomHexColor()}; height:${size}px; width:${size}px">${i}</div>`
+    );
+    boxString = box.join("");
+  }
+
+  return createBoxes(boxString);
+};
+
+createButton.addEventListener("click", getInputValue);
+
+function createBoxes(boxString) {
+  boxesPlace.innerHTML = "";
+  boxesPlace.insertAdjacentHTML("afterbegin", boxString);
+}
+
+function onClickResetValue(event) {
+  input.value = 0;
+  boxesPlace.innerHTML = "";
+}
+
+destroyButton.addEventListener("click", onClickResetValue);
